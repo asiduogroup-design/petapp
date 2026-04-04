ed8c62a9bf7d04d9b95da686bd72e1719dea777e
asiduogroup-design <asiduogroup@gmail.com>
2026-04-04 14:18:11 +0530

    Update User.js

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,35 +1,5 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      lowercase: true,
-    },
-    phone: {
-      type: String,
-      default: "+91 0000000000",
-      trim: true,
-    },
-    passwordHash: {
-      type: String,
-      required: true,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
-
-export default mongoose.model("User", userSchema);
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
