28fe04accf57b68a6b9b1c737f2f4866ebd84c04
sravanthi <sravanthikamani@gmail.com>
2026-04-06 15:28:50 +0530

    deleted unwanted routes

--- server/src/index.js
+++ server/src/index.js
@@ -3,13 +3,9 @@ import dotenv from "dotenv";
 import express from "express";
 
 import { connectDB } from "./config/db.js";
-import appointmentRoutes from "./routes/appointments.js";
 import authRoutes from "./routes/auth.js";
-import orderRoutes from "./routes/orders.js";
 
 import petRoutes from "./routes/pets.js";
-import userRoutes from "./routes/users.js";
-
 
 dotenv.config();
 
@@ -24,13 +20,8 @@ app.get("/api/health", (_req, res) => {
 });
 
 app.use("/api/auth", authRoutes);
-app.use("/api/orders", orderRoutes);
-app.use("/api/appointments", appointmentRoutes);
 
 app.use("/api/pets", petRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/orders", orderRoutes);
-
 app.use((err, _req, res, _next) => {
   console.error(err);
   res.status(500).json({ message: err.message || "Internal server error" });
